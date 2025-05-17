89f6754a9de35ec07d69a1aee51aa6fd67cedc79
Golen <colladoreden10@gmail.com>
2025-05-17 12:31:16 +0800

    feat:

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home():
-    return "Server is running! "
+    return "Server is runiing!"
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    data = request.json
+    msg = data.get("message", "").lower()
+    if msg == "hi":
+        return jsonify({"response": "hello"})
+    return jsonify({"response": f"I dont understand: {msg}"})
 
 if __name__ == "__main__":
     app.run(debug=True)
